c25ef17a8b412cffb129d06fa9ccf4682e34aa97
IshikawaUta <Jayaj7129@gmail.com>
2025-04-30 22:09:20 +0700

    update

--- app.py
+++ app.py
@@ -22,6 +22,10 @@ def before_request():
         session['cart'] = {}
     print(f"Sesi di awal request: {session}") # Tambahkan ini
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
 @app.route('/')
 def index():
     return render_template('index.html')
